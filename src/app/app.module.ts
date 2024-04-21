c517cad92b4f9ea68b849621454c8db6e1cb28aa
Khalid <khalidmajidali@live.com>
2024-04-21 23:38:58 +0500

    Avatar menu, Message Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
 import { AvatarModule } from 'primeng/avatar';
 import { AvatarGroupModule } from 'primeng/avatargroup';
+import { MenuModule } from 'primeng/menu';
+import { ToastModule } from 'primeng/toast';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { LeftNavComponent } from './left-nav/left-nav.component';
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, ButtonModule, AppRoutingModule, AvatarModule, AvatarGroupModule],
+	declarations: [AppComponent, TopNavComponent, LeftNavComponent],
+	imports: [BrowserModule, BrowserAnimationsModule, ButtonModule, AppRoutingModule, AvatarModule, AvatarGroupModule, MenuModule, ToastModule ],
 	providers: [],
 	bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+
+}
+
+
+
