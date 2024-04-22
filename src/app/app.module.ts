b266db927cd96cfe7bdfff8c521942416a948255
Kkhalid Ali <khalid.ali@gowithcore.com>
2024-04-22 22:32:41 +0500

    Tab Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonModule } from 'primeng/button';
 import { AvatarModule } from 'primeng/avatar';
 import { AvatarGroupModule } from 'primeng/avatargroup';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 import { MenuModule } from 'primeng/menu';
 import { ToastModule } from 'primeng/toast';
+
 import { TopNavComponent } from './top-nav/top-nav.component';
 import { LeftNavComponent } from './left-nav/left-nav.component';
 
 @NgModule({
 	declarations: [AppComponent, TopNavComponent, LeftNavComponent],
-	imports: [BrowserModule, BrowserAnimationsModule, ButtonModule, AppRoutingModule, AvatarModule, AvatarGroupModule, MenuModule, ToastModule ],
+	imports: [BrowserModule, BrowserAnimationsModule, ButtonModule, CardModule, AppRoutingModule, AvatarModule, AvatarGroupModule, MenuModule, ToastModule ],
 	providers: [],
 	bootstrap: [AppComponent],
 })
