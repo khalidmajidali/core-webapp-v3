a80c3fac1def7197ac1a675959fa77352ad176b3
Kkhalid Ali <khalid.ali@gowithcore.com>
2024-04-24 23:04:17 +0500

    Status bar and carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,126 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'core-webapp-v3';
+	title = 'core-webapp-v3';
+
+	responsiveOptions: any[] | undefined;
+
+	ngOnInit() {
+
+		this.responsiveOptions = [
+			{
+				breakpoint: '1400px',
+				numVisible: 3,
+				numScroll: 3,
+			},
+			{
+				breakpoint: '1220px',
+				numVisible: 2,
+				numScroll: 2,
+			},
+			{
+				breakpoint: '1100px',
+				numVisible: 1,
+				numScroll: 1,
+			},
+		];
+	}
+
+	jobstatuses = [
+		{
+			id: '1',
+			status: 'Open',
+			icon: 'pi pi-check-circle',
+			color: 'inprogress',
+			startDate: 'Dec 20, 2023, 5:11:33 PM',
+			endDate: 'Dec 20, 2023, 5:11:33 PM',
+			currentlyActive: false,
+		},
+		{
+			id: '2',
+			status: 'Assigned',
+			icon: 'pi pi-check-circle',
+			color: 'complete',
+			startDate: 'Dec 20, 2023, 5:11:33 PM',
+			endDate: 'Dec 20, 2023, 5:11:33 PM',
+			currentlyActive: false,
+		},
+		{
+			id: '3',
+			status: 'Rejected',
+			icon: 'pi pi-check-circle',
+			color: 'warning',
+			startDate: 'Dec 20, 2023, 5:11:33 PM',
+			endDate: 'Dec 20, 2023, 5:11:33 PM',
+			currentlyActive: false,
+		},
+		{
+			id: '4',
+			status: 'Open',
+			icon: 'pi pi-check-circle',
+			color: 'error',
+			startDate: 'Dec 20, 2023, 5:11:33 PM',
+			endDate: 'Dec 20, 2023, 5:11:33 PM',
+			currentlyActive: false,
+		},
+		{
+			id: '5',
+			status: 'Assigned',
+			icon: 'pi pi-check-circle',
+			color: 'inprogress',
+			startDate: 'Dec 20, 2023, 5:11:33 PM',
+			endDate: 'Dec 20, 2023, 5:11:33 PM',
+			currentlyActive: false,
+		},
+		{
+			id: '6',
+			status: 'Rejected',
+			icon: 'pi pi-check-circle',
+			color: 'inprogress',
+			startDate: 'Dec 20, 2023, 5:11:33 PM',
+			endDate: 'Dec 20, 2023, 5:11:33 PM',
+			currentlyActive: true,
+		},
+		{
+			id: '7',
+			status: 'Open',
+			icon: 'pi pi-check-circle',
+			color: 'inprogress',
+			startDate: 'Dec 20, 2023, 5:11:33 PM',
+			endDate: 'Dec 20, 2023, 5:11:33 PM',
+			currentlyActive: false,
+		},
+		{
+			id: '7',
+			status: 'Open',
+			icon: 'pi pi-check-circle',
+			color: 'inprogress',
+			startDate: 'Dec 20, 2023, 5:11:33 PM',
+			endDate: 'Dec 20, 2023, 5:11:33 PM',
+			currentlyActive: false,
+		},
+		{
+			id: '7',
+			status: 'Open',
+			icon: 'pi pi-check-circle',
+			color: 'inprogress',
+			startDate: 'Dec 20, 2023, 5:11:33 PM',
+			endDate: 'Dec 20, 2023, 5:11:33 PM',
+			currentlyActive: false,
+		},
+		{
+			id: '7',
+			status: 'Open',
+			icon: 'pi pi-check-circle',
+			color: 'inprogress',
+			startDate: 'Dec 20, 2023, 5:11:33 PM',
+			endDate: 'Dec 20, 2023, 5:11:33 PM',
+			currentlyActive: false,
+		},
+	]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,17 @@ import { AvatarModule } from 'primeng/avatar';
 import { AvatarGroupModule } from 'primeng/avatargroup';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { CarouselModule } from 'primeng/carousel';
 import { MenuModule } from 'primeng/menu';
 import { ToastModule } from 'primeng/toast';
+import { TooltipModule } from 'primeng/tooltip';
 
 import { TopNavComponent } from './top-nav/top-nav.component';
 import { LeftNavComponent } from './left-nav/left-nav.component';
 
 @NgModule({
 	declarations: [AppComponent, TopNavComponent, LeftNavComponent],
-	imports: [BrowserModule, BrowserAnimationsModule, ButtonModule, CardModule, AppRoutingModule, AvatarModule, AvatarGroupModule, MenuModule, ToastModule ],
+	imports: [BrowserModule, BrowserAnimationsModule, ButtonModule, CardModule, CarouselModule, AppRoutingModule, AvatarModule, AvatarGroupModule, MenuModule, ToastModule, TooltipModule ],
 	providers: [],
 	bootstrap: [AppComponent],
 })
